b6f5a79986f021191154378f69ded8357d1c71b8
monk3yd <jegajardog@gmail.com>
2022-09-15 00:59:06 -0300

    First commit, some pseudocode in comments

---
+++ main.py
@@ -0,0 +1,12 @@
+def main():
+    # Load mp3 format audio file
+    # Convert mp3 to wav format
+    # Load wav format audio file
+    # Feed file to speech recognition system
+    # Convert audio file to text
+    # Save text to txt file
+    ...
+
+
+if __name__ == "__main__":
+    main()
